02c47b232646dfc61bde265e0317825d0d8026d9
CallenThomason <thomasoncallen@gmail.com>
2026-09-09 11:55:21 -0700

    Valery review c #2

--- Program.cs
+++ Program.cs
@@ -2,8 +2,12 @@
 //09/09/2026
 //Mini Challenge #2 Add 2 Numbers
 //I used variables to store numbers that user input as strings, then converted it to integers and added them together
-//Peer review name:
-//Review:
+//Peer review name: Callen Thomason
+//Review: Code looks great! Variables names are clear, and concatination looks good. 
+// Maybe try interpolation instead of concatination next time.
+//conversions are great! 
+//flow chart looks good, and your code matches. 
+//Code works as intended. 
 
 Console.WriteLine("Please enter a number");
 string userNum1 = Console.ReadLine();
